1465f0b6e8a91724c0b0727c6d97ea4e03be7ec5
v-inoue <tomohiro1020.i@gmail.com>
2025-12-10 15:10:11 +0900

    Assignment2

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { VStack } from "@chakra-ui/react";
 import Home from "./pages/Assignment0";
 import Assignment1 from "./pages/Assignment1";
+import Assignment2 from "./pages/Assignment2";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/assignment1" element={<Assignment1 />} />
+        <Route path="/assignment2" element={<Assignment2 />} />
       </Routes>
     </VStack>
   );
